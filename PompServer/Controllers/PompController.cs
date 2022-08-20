f5b5dc2766023f401af437385fb4c26109271cfa
Matts <vdpmatts@gmail.com>
2022-08-20 15:41:52 +0200

    Add .net projects

---
+++ PompServer/Controllers/PompController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PompServer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PompController : ControllerBase
+{
+    private readonly ILogger<PompController> _logger;
+
+    public PompController(ILogger<PompController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetPomp")]
+    public string Index() {
+        return "Hallo";
+    }
+}
