e8943f880af2f88ebf30fbc1500b8d7675b4a6af
Matts <vdpmatts@gmail.com>
2022-08-22 21:15:34 +0200

    addd pumpService

--- PompServer/Services/PumpService.cs
+++ PompServer/Services/PumpService.cs
@@ -1,9 +1,23 @@
-﻿using System;
+﻿using PompServer.Models;
+using System;
 
+namespace PompServer.Services;
 
 public class PumpService
 {
-	public PumpService()
+	private CommandExecutor commandExecutor;
+	public PumpService(ILogger<PumpService> logger)
 	{
+		commandExecutor = new CommandExecutor(new Pump(), logger);
+	}
+
+	public List<Command> GetCommands()
+	{
+		return commandExecutor.GetCommands();
+	}
+
+	public void AddCommand(Command command)
+	{
+		commandExecutor.AddCommand(command);
 	}
 }
