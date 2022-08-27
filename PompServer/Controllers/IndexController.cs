fd60722ea5dee6c4e493a3d94d0c4b004c25a63f
Matts <vdpmatts@gmail.com>
2022-08-27 14:29:52 +0200

    add redirect

---
+++ PompServer/Controllers/IndexController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+using PompServer.Hubs;
+using PompServer.Models;
+using PompServer.Services;
+
+namespace PompServer.Controllers;
+
+[ApiController]
+[Route("")]
+public class IndexController : ControllerBase
+{
+    private readonly ILogger<IndexController> _logger;
+
+    public IndexController(
+        ILogger<IndexController> logger
+        )
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return Redirect("/index.html");
+    }
+}
